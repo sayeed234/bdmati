de0755b1975745f1176de175bc24e21c6f73970f
Sayeed <abu.sayeed.diu@gmail.com>
2020-09-13 17:07:25 +0600

    API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/users', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:api')->get('/users', function (Request $request) {
+    return $request->user();
+});
